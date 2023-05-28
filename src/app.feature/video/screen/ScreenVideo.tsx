7b9f7f81c5feaa5e748016c2bd712f2abf4d43d7
caseBread <kgu0515@gmail.com>
2023-05-28 23:10:57 +0900

    feat: add video

--- src/app.feature/video/screen/ScreenVideo.tsx
+++ src/app.feature/video/screen/ScreenVideo.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
 const ScreenVideo = () => {
-  return <div>screenVideo</div>;
+  return (
+    <StyledWrapper>
+      <video autoPlay loop muted>
+        <source src="/videos/slash23.mp4" type="video/mp4" />
+      </video>
+    </StyledWrapper>
+  );
 };
 
 export default ScreenVideo;
+
+const StyledWrapper = styled.div`
+  video {
+    width: 100vw;
+  }
+`;
