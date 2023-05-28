34b77d020b60f57ce61dfe879f6927269df0b16e
caseBread <kgu0515@gmail.com>
2023-05-28 23:23:18 +0900

    feat : add useScroll hook '

--- src/app.feature/video/screen/ScreenVideo.tsx
+++ src/app.feature/video/screen/ScreenVideo.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
+import useScroll from "../../../app.module/hooks/useScroll";
 
 const ScreenVideo = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const scrollY = useScroll();
+
+  useEffect(() => {
+    const video = videoRef.current as HTMLVideoElement;
+
+    if (scrollY > 200) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }, [scrollY]);
+
   return (
     <StyledWrapper>
-      <video autoPlay loop muted>
+      <video ref={videoRef} muted>
         <source src="/videos/slash23.mp4" type="video/mp4" />
       </video>
     </StyledWrapper>
---
+++ src/app.module/hooks/useScroll.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useScroll = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    // 스크롤 이벤트 리스너 등록
+    window.addEventListener("scroll", handleScroll);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollY;
+};
+
+export default useScroll;
